c613d5ad17fc31e9780bf8ba8d2df00c31dceb7c
shiva <shivain22@gmail.com>
2023-01-10 16:06:27 +0530

    Tab data form.

--- wicket-training-spring-boot/src/main/java/com/gel/wicket_training/spring_boot/AjaxFormInTabbedPanel.java
+++ wicket-training-spring-boot/src/main/java/com/gel/wicket_training/spring_boot/AjaxFormInTabbedPanel.java
@@ -7,17 +7,25 @@ import org.apache.wicket.extensions.ajax.markup.html.tabs.AjaxTabbedPanel;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 
 public class AjaxFormInTabbedPanel extends HomePage
 {
+	
+	
+	List<ITab> tabs = new ArrayList<>();
+	AjaxTabbedPanel tabbedPanel  = new AjaxTabbedPanel<>("tabs", tabs);
 	/**
 	 * Constructor
 	 */
 	public AjaxFormInTabbedPanel()
 	{
-		List<ITab> tabs = new ArrayList<>();
+		
 		tabs.add(new AbstractTab(new Model<>("Details"))
 		{
 			@Override
@@ -61,22 +69,68 @@ public class AjaxFormInTabbedPanel extends HomePage
 			}
 		});
 
-		add(new AjaxTabbedPanel<>("tabs", tabs));
+		add(tabbedPanel);
 	}
 
-	private static class PersonDetail extends Panel
+	private class PersonDetail extends Panel
 	{
 		public PersonDetail(String id)
 		{
 			super(id);
+			TextField firstName = new TextField<>("txtFirstName",Model.of(""));
+			TextField lastName = new TextField<>("txtLastName",Model.of(""));
+			Form tabAddForm = new Form<>("tabPersonDetailsAddForm");
+			tabAddForm.add(new Label("lblFirstName",Model.of("First Name")));
+			tabAddForm.add(new Label("lblLastName",Model.of("Last Name")));
+			tabAddForm.add(firstName);
+			tabAddForm.add(lastName);
+			tabAddForm.add(new Button("btnGoToMobileNumber") {
+
+				@Override
+				public void onSubmit() {
+					System.out.println(firstName.getInput()+"===="+lastName.getInput());
+					tabbedPanel.setSelectedTab(1);
+					super.onSubmit();
+				}
+				
+				
+			}.setDefaultFormProcessing(false));
+			add(tabAddForm);
 		}
 	}
 
-	private static class PersonMobileNumber extends Panel
+	private  class PersonMobileNumber extends Panel
 	{
 		public PersonMobileNumber(String id)
 		{
 			super(id);
+			TextField mobileNumber = new TextField<>("txtMobileNumber",Model.of(""));
+			Form tabMobileNumberAddForm = new Form<>("tabMobileNumberAddForm");
+			tabMobileNumberAddForm.add(new Label("lblMobileNumber",Model.of("Mobile Number")));
+			tabMobileNumberAddForm.add(mobileNumber);
+			tabMobileNumberAddForm.add(new Button("btnGoToEmail") {
+
+				@Override
+				public void onSubmit() {
+					System.out.println(mobileNumber.getInput());
+					tabbedPanel.setSelectedTab(2);
+					super.onSubmit();
+				}
+				
+				
+			}.setDefaultFormProcessing(false));
+			tabMobileNumberAddForm.add(new Button("btnGoToPersonDetail") {
+
+				@Override
+				public void onSubmit() {
+					System.out.println(mobileNumber.getInput());
+					tabbedPanel.setSelectedTab(0);
+					super.onSubmit();
+				}
+				
+				
+			}.setDefaultFormProcessing(false));
+			add(tabMobileNumberAddForm);
 		}
 	}
 
